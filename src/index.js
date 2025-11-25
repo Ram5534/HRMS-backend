7747183e128ed3ec82ad2dcd317ae57d6377f9a1
Ram5534 <kurakularammohanrao@gmail.com>
2025-11-25 19:58:36 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ const app = express();
 const { sequelize } = require("./models");
 const cors = require('cors')
 
-app.use(express.json());
 app.use(cors())
+app.use(express.json());
+
 
 // Auth & Employee routes
 app.use("/api/auth", require("./routes/auth"));
@@ -15,6 +16,10 @@ app.use("/api/teams", require("./routes/teams"));
 
 // Logs
 app.use("/api/logs", require("./routes/logs"));
+
+app.get('/',(req,res)=>{
+  res.send("Welcome to Hrms")
+})
 
 sequelize.sync({ alter: true }).then(() => {
   app.listen(8080, () => console.log("Server running on 8080"));
